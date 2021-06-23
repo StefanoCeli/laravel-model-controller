57c761e129554670a3def0d0f11662cc5a708814
StefanoCeli <stefanopsn91@gmail.com>
2021-06-23 18:45:18 +0200

    fine bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,15 @@
 
         <ul>
             @foreach ($movies as $movie )
-                <li><strong>Titolo: </strong>{{ $movie['title'] }}</li>
-                <li><strong>Titolo originale: </strong>{{ $movie['original_title'] }}</li>
-                <li><strong>Nazionalità: </strong>{{ $movie['nationality'] }}</li>
-                <li><strong>Data: </strong>{{ $movie['date'] }}</li>
-                <li><strong>Voto: </strong>{{ $movie['vote'] }}</li>
-                <br>
+                
+            <div>
+                <li><span>Titolo: </span>{{ $movie['title'] }}</li>
+                <li><span>Titolo originale: </span>{{ $movie['original_title'] }}</li>
+                <li><span>Nazionalità: </span>{{ $movie['nationality'] }}</li>
+                <li><span>Data: </span>{{ $movie['date'] }}</li>
+                <li><span>Voto: </span>{{ $movie['vote'] }}</li>
+            </div>    
+
             @endforeach
         </ul>
 
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="container text-center p-3">
+        <h1>Database Film</h1>
+    </div>
+</header>
